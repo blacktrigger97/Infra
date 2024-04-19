75eaa2b17d4c9aca05fa6945187db8c8d3f279ef
blacktrigger97 <blacktrigger97@gmail.com>
2024-04-19 12:02:02 +0530

    hue initital setup

--- hdc/postgresql/init.sql
+++ hdc/postgresql/init.sql
@@ -1,1 +1,3 @@
 SELECT 'CREATE DATABASE nessie_gc' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'nessie_gc')\gexec
+
+SELECT 'CREATE DATABASE hue' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'hue')\gexec
