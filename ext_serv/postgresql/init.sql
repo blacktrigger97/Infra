c34130937f927783c3ac7b53d0f61fedd899ccc4
blacktrigger97 <blacktrigger97@gmail.com>
2024-08-24 13:14:35 +0530

    baseimage update

--- ext_serv/postgresql/init.sql
+++ ext_serv/postgresql/init.sql
@@ -3,3 +3,5 @@ SELECT 'CREATE DATABASE nessie_gc' WHERE NOT EXISTS (SELECT FROM pg_database WHE
 SELECT 'CREATE DATABASE hue' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'hue')\gexec
 
 SELECT 'CREATE DATABASE trino' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'trino')\gexec
+
+SELECT 'CREATE DATABASE trino' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'airflow')\gexec
