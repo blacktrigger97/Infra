3b6d1aedd7b8f312a786172d4509cb9b3d1ddad7
blacktrigger97 <blacktrigger97@gmail.com>
2024-08-24 20:52:09 +0530

    postgresql upd

--- ext_serv/postgresql/init.sql
+++ ext_serv/postgresql/init.sql
@@ -4,4 +4,4 @@ SELECT 'CREATE DATABASE hue' WHERE NOT EXISTS (SELECT FROM pg_database WHERE dat
 
 SELECT 'CREATE DATABASE trino' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'trino')\gexec
 
-SELECT 'CREATE DATABASE trino' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'airflow')\gexec
+SELECT 'CREATE DATABASE airflow' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'airflow')\gexec
