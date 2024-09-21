ffc30aa7e2309ca2874f57f0bf318d7be3816798
blacktrigger97 <blacktrigger97@gmail.com>
2024-09-21 20:29:43 +0530

    pgadmin4 addition

--- ext_serv/postgresql/init.sql
+++ ext_serv/postgresql/init.sql
@@ -5,3 +5,5 @@ SELECT 'CREATE DATABASE hue' WHERE NOT EXISTS (SELECT FROM pg_database WHERE dat
 SELECT 'CREATE DATABASE trino' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'trino')\gexec
 
 SELECT 'CREATE DATABASE airflow' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'airflow')\gexec
+
+SELECT 'CREATE DATABASE logs' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'logs')\gexec
