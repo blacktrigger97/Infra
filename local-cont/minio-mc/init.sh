49025c60b7d7971b853cdd9df361311f230a02ae
blacktrigger97 <blacktrigger97@gmail.com>
2025-06-27 15:32:47 +0530

    upd

--- local-cont/minio-mc/init.sh
+++ local-cont/minio-mc/init.sh
@@ -11,9 +11,9 @@ minio2.bdc.home
 minio3.bdc.home
 '
 # like: minio1 minio2 ...
-# INSTANCES=`for host in $HOSTS; do echo $host | cut -d "@" -f 2 | cut -d ":" -f 1; done`
-# REPLICATED_INSTANCES=''
-# NON_REPLICATED_INSTANCES=''
+# INSTANCES=`for host in $HOSTS; do echo $host; done`
+REPLICATED_INSTANCES=''
+NON_REPLICATED_INSTANCES=''
 
 # waiting for all hosts to start responding
 for host in $HOSTS
@@ -25,32 +25,32 @@ do
 done
 
 # detect the replicated/non-replicated instances
-# for instance in $INSTANCES;
-# do
-#   mc admin replicate info $instance #| grep http > /dev/null
-#   if [ $? == 0 ];
-#   then
-#     REPLICATED_INSTANCES="${REPLICATED_INSTANCES} ${instance}"
-#   else
-#     NON_REPLICATED_INSTANCES="${NON_REPLICATED_INSTANCES} ${instance}"
-#     echo "non-replicated instance found: $instance"
-#   fi
-# done
+for instance in $HOSTS;
+do
+  mc admin replicate info $instance #| grep http > /dev/null
+  if [ $? == 0 ];
+  then
+    REPLICATED_INSTANCES="${REPLICATED_INSTANCES} ${instance}"
+  else
+    NON_REPLICATED_INSTANCES="${NON_REPLICATED_INSTANCES} ${instance}"
+    echo "non-replicated instance found: $instance"
+  fi
+done
 
-# # decide wether there is an instance to be added to replication or not
-# if [ "x${NON_REPLICATED_INSTANCES}" != "x" ];
-# then
-#   mc admin replicate add $REPLICATED_INSTANCES $NON_REPLICATED_INSTANCES
-#   echo -e "\n************************************************"
-#   echo -e "****** new instances added to replication ******"
-#   echo -e "************************************************\n"
-#   echo "new replicated instances:"
-#   echo $NON_REPLICATED_INSTANCES
-# else
-#   echo -e "\n***************************************"
-#   echo -e "****** replication already setup ******"
-#   echo -e "***************************************\n"
-# fi
+# decide wether there is an instance to be added to replication or not
+if [ "x${NON_REPLICATED_INSTANCES}" != "x" ];
+then
+  mc admin replicate add $REPLICATED_INSTANCES $NON_REPLICATED_INSTANCES
+  echo -e "\n************************************************"
+  echo -e "****** new instances added to replication ******"
+  echo -e "************************************************\n"
+  echo "new replicated instances:"
+  echo $NON_REPLICATED_INSTANCES
+else
+  echo -e "\n***************************************"
+  echo -e "****** replication already setup ******"
+  echo -e "***************************************\n"
+fi
 
 echo -e "\n*****************************************"
 echo -e "****** started bucket creation process ******"
@@ -59,7 +59,6 @@ mc mb minio/dos
 if [ $? == 0 ];
 then
   mc policy set public minio/dos
-  mc admin replicate add minio1 minio2 minio2
 else
   echo "Bucket Creation Failed."
   exit 1
