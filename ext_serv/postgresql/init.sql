243da587c977e3cc64fc0ef28cbaeba62b89e7c7
blacktrigger97 <blacktrigger97@gmail.com>
2025-06-30 12:23:12 +0530

    upd

--- ext_serv/postgresql/init.sql
+++ ext_serv/postgresql/init.sql
@@ -1,5 +1,3 @@
--- SELECT 'CREATE DATABASE nessie_gc' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'nessie_gc')\gexec
-
 SELECT 'CREATE DATABASE hue' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'hue')\gexec
 
 SELECT 'CREATE DATABASE trino' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'trino')\gexec
