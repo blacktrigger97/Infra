836bc69b07a9d981b8694848aaff40f8e6797459
blacktrigger97 <blacktrigger97@gmail.com>
2025-12-22 23:17:04 +0530

    upd 2

--- db/postgresql/init.sql
+++ db/postgresql/init.sql
@@ -1,8 +1,10 @@
 CREATE ROLE hue LOGIN PASSWORD 'abcd1234' \gexec
+CREATE ROLE airflow LOGIN PASSWORD 'abcd1234' \gexec
 
 SELECT 'CREATE DATABASE bdc' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'bdc')\gexec
 
 GRANT CONNECT ON DATABASE bdc TO hue \gexec
+GRANT CONNECT ON DATABASE bdc TO airflow \gexec
 
 \c bdc \gexec
 
@@ -22,6 +24,10 @@ CREATE SCHEMA IF NOT EXISTS hue \gexec
 -- ALTER DEFAULT PRIVILEGES IN SCHEMA hue GRANT ALL PRIVILEGES ON SEQUENCES TO hue;
 
 GRANT ALL ON SCHEMA hue TO hue \gexec
+GRANT ALL ON SCHEMA airflow TO airflow \gexec
+
+ALTER USER hue SET search_path = hue \gexec
+ALTER USER airflow SET search_path = airflow \gexec
 
 REVOKE ALL ON SCHEMA public FROM public \gexec
 
