8ca55ccb3116b0e54d0407fbba1a0a2c5e0aa6dd
blacktrigger97 <blacktrigger97@gmail.com>
2025-12-27 01:12:13 +0530

    upd 2

--- ext_serv/hue/startup.sh
+++ ext_serv/hue/startup.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-# export PYTHON_VER=python3.11
-# export ROOT=/usr/share/hue/
-# ./build/env/bin/pip3.11 install --upgrade pip
-# ./build/env/bin/pip3.11 install ksql
+./build/env/bin/pip3.11 install --upgrade pip
+./build/env/bin/pip3.11 install ksql
 ./build/env/bin/hue migrate
 ./build/env/bin/supervisor
