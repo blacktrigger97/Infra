d21f31813412a8ddbc440e333f6d865f378648e7
blacktrigger97 <blacktrigger97@gmail.com>
2026-01-03 16:34:52 +0530

    upd 2

--- db/postgresql/init.sql
+++ db/postgresql/init.sql
@@ -1,18 +1,18 @@
 CREATE ROLE hue LOGIN PASSWORD 'abcd1234' \gexec
-CREATE ROLE kafka LOGIN PASSWORD 'abcd1234' \gexec
+-- CREATE ROLE kafka LOGIN PASSWORD 'abcd1234' \gexec
 CREATE ROLE airflow LOGIN PASSWORD 'abcd1234' \gexec
 
 SELECT 'CREATE DATABASE bdc' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'bdc')\gexec
 
 GRANT CONNECT ON DATABASE bdc TO hue \gexec
-GRANT CONNECT ON DATABASE bdc TO kafka \gexec
+-- GRANT CONNECT ON DATABASE bdc TO kafka \gexec
 GRANT CONNECT ON DATABASE bdc TO airflow \gexec
 
 \c bdc \gexec
 
 CREATE SCHEMA IF NOT EXISTS nessie \gexec
 
-CREATE SCHEMA IF NOT EXISTS kafka \gexec
+-- CREATE SCHEMA IF NOT EXISTS kafka \gexec
 
 CREATE SCHEMA IF NOT EXISTS airflow \gexec
 
@@ -28,11 +28,11 @@ CREATE SCHEMA IF NOT EXISTS hue \gexec
 -- ALTER DEFAULT PRIVILEGES IN SCHEMA hue GRANT ALL PRIVILEGES ON SEQUENCES TO hue;
 
 GRANT ALL ON SCHEMA hue TO hue \gexec
-GRANT ALL ON SCHEMA kafka TO kafka \gexec
+-- GRANT ALL ON SCHEMA kafka TO kafka \gexec
 GRANT ALL ON SCHEMA airflow TO airflow \gexec
 
 ALTER USER hue SET search_path = hue \gexec
-ALTER USER kafka SET search_path = kafka \gexec
+-- ALTER USER kafka SET search_path = kafka \gexec
 ALTER USER airflow SET search_path = airflow \gexec
 
 REVOKE ALL ON SCHEMA public FROM public \gexec
