915faae4730ab18fd688979b70307d2bf1a6d55b
blacktrigger97 <blacktrigger97@gmail.com>
2026-01-12 23:11:34 +0530

    upd

--- ext_serv/hue/startup.sh
+++ ext_serv/hue/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 ./build/env/bin/pip3.11 install --upgrade pip
-./build/env/bin/pip3.11 install ksql trino-client[iceberg,s3]
+./build/env/bin/pip3.11 install ksql trino[iceberg,s3]
 ./build/env/bin/hue migrate
 ./build/env/bin/supervisor
