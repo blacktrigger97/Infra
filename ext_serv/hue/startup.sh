0f67646962f7582365fc1a793d72e852ad01f258
blacktrigger97 <blacktrigger97@gmail.com>
2026-01-12 23:23:46 +0530

    upd

--- ext_serv/hue/startup.sh
+++ ext_serv/hue/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 ./build/env/bin/pip3.11 install --upgrade pip
-./build/env/bin/pip3.11 install ksql trino[sqlalchemy]
+./build/env/bin/pip3.11 install ksql trino
 ./build/env/bin/hue migrate
 ./build/env/bin/supervisor
